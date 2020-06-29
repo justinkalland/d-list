c2cb18f67ef28f116142615b1fa2a5f451aefa74
Justin Kalland <justin@kalland.com>
2020-06-29 18:52:45 +0200

    feat: add getDirsSync
    Works like fs.readdir(path) but only returns directories (not files)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export default function (): void {
-  // welcome
+import fs from 'fs'
+import nodePath from 'path'
+
+const getDirsSync = (path): String[] => {
+  const readResult = fs.readdirSync(path)
+  const dirs = readResult.filter(f => fs.statSync(nodePath.join(path, f)).isDirectory())
+
+  return dirs
+}
+
+export default {
+  getDirsSync
 }
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,5 +1,25 @@
-describe('example', () => {
-  it('works', () => {
-    expect(true).toEqual(true)
+import path from 'path'
+import dl from '../'
+
+const relativePath = 'src/tests/test-dir'
+const absolutePath = path.join(__dirname, 'test-dir/')
+
+describe('gets directories', () => {
+  it('sync with relative path', () => {
+    const dirs = dl.getDirsSync(relativePath)
+    expect(dirs).toEqual([
+      'dir-1',
+      'dir-2',
+      'dir-3'
+    ])
+  })
+
+  it('sync with absolute path', () => {
+    const dirs = dl.getDirsSync(absolutePath)
+    expect(dirs).toEqual([
+      'dir-1',
+      'dir-2',
+      'dir-3'
+    ])
   })
 })
