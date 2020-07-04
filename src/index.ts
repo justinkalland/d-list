ddc87656a636c541dc9d67493bc5a11e4ac01c3a
Justin Kalland <justin@kalland.com>
2020-07-04 14:51:32 +0200

    fix: change returned types from String to string
    All functions are typed as returning String[]. This changes it to string[]. Using String was an accident and caused some projects to have type errors.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import fs from 'fs'
 import nodePath from 'path'
 
-const getDirsSync = (path: string): String[] => {
+const getDirsSync = (path: string): string[] => {
   const readResult = fs.readdirSync(path)
   const dirs = readResult.filter(f => fs.statSync(nodePath.join(path, f)).isDirectory())
 
   return dirs
 }
 
-const getDirs = async (path: string): Promise<String[]> => {
+const getDirs = async (path: string): Promise<string[]> => {
   const readResult = await fs.promises.readdir(path)
   const stats = await Promise.all(
     readResult.map(f => fs.promises.stat(nodePath.join(path, f))) // eslint-disable-line @typescript-eslint/promise-function-async
@@ -18,14 +18,14 @@ const getDirs = async (path: string): Promise<String[]> => {
   return dirs
 }
 
-const getFilesSync = (path: string): String[] => {
+const getFilesSync = (path: string): string[] => {
   const readResult = fs.readdirSync(path)
   const files = readResult.filter(f => fs.statSync(nodePath.join(path, f)).isFile())
 
   return files
 }
 
-const getFiles = async (path: string): Promise<String[]> => {
+const getFiles = async (path: string): Promise<string[]> => {
   const readResult = await fs.promises.readdir(path)
   const stats = await Promise.all(
     readResult.map(f => fs.promises.stat(nodePath.join(path, f))) // eslint-disable-line @typescript-eslint/promise-function-async
